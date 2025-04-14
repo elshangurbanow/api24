22eadf224b87caaafabc4fb68e0c2ec6f705e933
elshangurbanow <elsenqurbanov50@gmail.com>
2025-04-14 23:18:58 +0000

    chancged

---
+++ api/models.py
@@ -0,0 +1,153 @@
+class News:
+
+    def __init__(self, tit, des, img):
+
+        self.tit = tit
+        self.des = des
+        self.img = img
+
+news1 = News(
+    "PS5 price rises substantially in UK and Europe",
+    "Sony has raised the price of most of its PlayStation 5 console hardware in the UK, Europe, Australia, and New Zealand by 10-15 percent, blaming “a challenging economic environment.” With rising US import costs thanks to tariffs likely a contributing factor, an equivalent price rise in the US is probably on the way.",
+    "https://picsum.photos/400?random=1"
+)
+
+news2 = News(
+    "How the Switch 2 compares to the ROG Ally",
+    "Nintendo’s new handheld attempts to be a PC with its mouse-like controls, but the ROG Ally is more versatile.",
+    "https://picsum.photos/400?random=2"
+)
+
+news3 = News(
+    "Science Saru’s The Ghost in the Shell series gets a new teaser trailer",
+    "The 2026 series’ teaser is all concept art that looks a lot like the original manga.",
+    "https://picsum.photos/400?random=3"
+)
+
+news4 = News(
+    "Razer’s PC-to-mobile streaming platform is now available",
+    "Razer has finally launched its platform for streaming PC games to mobile devices at their native screen resolutions, aspect ratios, and refresh rates. Razer PC Remote Play is now available for download on the App Store and Google Play and is compatible with Windows, Apple, and Android mobile devices running at least Windows 11, iOS 18, and Android 14, respectively.",
+    "https://picsum.photos/400?random=4"
+)
+
+news5 = News(
+    "DOGE staffers are listed in the FCC directory",
+    "Elon Musk’s Department of Government Efficiency (DOGE) has infiltrated the Federal Communications Commission (FCC), an agency that has a say over resources Musk needs or could benefit from for some of his private sector business, The Verge has learned.",
+    "https://picsum.photos/400?random=5"
+)
+
+news6 = News(
+    "Hacking GoPros to help save the Atlantic’s rarest bird",
+    "The Bermuda petrel was thought to be extinct for over 300 years. DIY conservation tech is helping to bring it back from the brink.",
+    "https://picsum.photos/400?random=6"
+)
+
+news7 = News(
+    "Loss of Pulse alerts are rolling out on Pixel Watch 3",
+    "You’ll have to wait until you see a prompt on the watch to enable the opt-in feature. But don’t worry if you don’t see it just yet. While rollout starts today, Google spokesperson Bridget Starkey told The Verge in an email that users may see it over the next couple of weeks.",
+    "https://picsum.photos/400?random=7"
+)
+
+news8 = News(
+    "Starlink competition is ramping up in Ukraine",
+    "Satellite network company Eutelsat could be Ukraines alternative to Elon Musk-owned SpaceX.",
+    "https://picsum.photos/400?random=8"
+)
+
+news9 = News(
+    "Honda is sending its hydrogen tech to space",
+    "The automaker is teaming up with space tech companies Sierra Space and Tec-Masters to test its high-differential pressure water electrolysis system on the International Space Station. The test is part of Honda’s vision to support life on the Moon and elsewhere in space using regenerative fuel cell technology that continuously produces hydrogen, oxygen, and electricity.",
+    "https://picsum.photos/400?random=9"
+)
+
+news10 = News(
+    "Amazon is ready to launch its Starlink competitor",
+    "The first batch of 27 Project Kuiper space internet satellites are scheduled to launch next week. Amazon has secured 80 such launch missions that will each deliver dozens of satellites into low earth orbit (LEO) to create a constellation capable of competing with Elon Musk’s Starlink juggernaut. Amazon says it expects to begin offering high-speed, low-latency internet service “later this year.”",
+    "https://picsum.photos/400?random=10"
+)
+
+news11 = News(
+    "Can I interest you in an app that tracks your driving behavior?",
+    "The AAA Foundation for Traffic Safety just published a study that found that using smartphone apps to monitor driver behavior, and then providing summaries of that behavior via text message or a data dashboard, leads to safer driving.",
+    "https://picsum.photos/400?random=11"
+)
+
+news12 = News(
+    "Can I interest you in an app that tracks your driving behavior?",
+    "The AAA Foundation for Traffic Safety just published a study that found that using smartphone apps to monitor driver behavior, and then providing summaries of that behavior via text message or a data dashboard, leads to safer driving.",
+    "https://picsum.photos/400?random=12"
+)
+
+news13 = News(
+    "Rural fiber internet expansion is at risk as Trump administration holds funds",
+    "Fiber internet providers are worried they won’t see the funds promised under a Biden-era initiative that would bring reliable internet service to rural areas. Louisiana fiber internet provider Cajun Broadband was granted $33 million as part of the plan, but the “money isn’t flowing” and concerns are growing over whether the new administration will prioritize satellite internet services, like Elon Musk’s Starlink.",
+    "https://picsum.photos/400?random=13"
+)
+
+news14 = News(
+    "Hundreds of scientists accuse Donald Trump of censorship",
+    "The US risks losing a generation of scientists under Donald Trump’s draconian targeting of universities and federal agencies, scientists warn in an open letter.",
+    "https://picsum.photos/400?random=14"
+)
+
+news15 = News(
+    "The space race heats up: all the news on the latest rocket launches",
+    "The commercial space industry is booming, and it’s only expected to keep growing as more billionaire-led companies send rockets into the sky. Elon Musk’s SpaceX continues to dominate the space industry with dozens of launches each year, but it could soon have some serious competition with Jeff Bezos’ Blue Origin launching its rival New Glenn rocket for the first time in January.",
+    "https://picsum.photos/400?random=15"
+)
+
+news16 = News(
+    "The Trump administration could green-light deep-sea mining for the first time",
+    "A deep-sea mining company is turning to President Donald Trump to help it extract battery materials.",
+    "https://picsum.photos/400?random=16"
+)
+
+news17 = News(
+    "Katy Perry launched into space with Blue Origin",
+    "Blue Origin’s NS-31 mission has an all-female crew that also includes scientists, journalists, and a film producer.",
+    "https://picsum.photos/400?random=17"
+)
+
+news18 = News(
+    "Trump says the future of AI is powered by coal",
+    "His latest executive order directs the government to start using “beautiful, clean coal” to power AI data centers.",
+    "https://picsum.photos/400?random=18"
+)
+
+news19 = News(
+    "Apple’s complicated plan to improve its AI while protecting privacy",
+    "Apple says it’s found a way to make its AI models better without training on its users’ data or even copying it from their iPhones and Macs. In a blog post first reported on by Bloomberg, the company outlined its plans to have devices compare a synthetic dataset to samples of recent emails or messages from users who have opted into its Device Analytics program.",
+    "https://picsum.photos/400?random=19"
+)
+
+news20 = News(
+    "OpenAI debuts its GPT-4.1 flagship AI model",
+    "OpenAI has introduced GPT-4.1, a successor to the GPT-4o multimodal AI model launched by the company last year. During a livestream on Monday, OpenAI said GPT-4.1 has an even larger context window and is better than GPT-4o in “just about every dimension,” with big improvements to coding and instruction following.",
+    "https://picsum.photos/400?random=20"
+)
+
+news21 = News(
+    "Meta AI will soon train on EU users’ data",
+    "Meta has announced that it’s preparing to train AI on the data of EU users of its apps, including Facebook and Instagram. The company says that includes things like public posts, comments, and their chat history with Meta AI, but won’t include “private messages with friends and family.” It also only applies to those who are over 18, the company says.",
+    "https://picsum.photos/400?random=21"
+)
+
+news22 = News(
+    "Nvidia starts producing its Blackwell AI chip at TSMC’s Arizona plant",
+    "Nvidia has brought some of its chip production stateside. On Monday, Nvidia announced that it has started producing its Blackwell AI GPUs at TSMC’s plant in Phoenix, Arizona, while companies within the state package and test them.",
+    "https://picsum.photos/400?random=22"
+)
+
+news23 = News(
+    "YouTube’s new AI tool generates free background music for videos",
+    "YouTube is giving creators a new AI tool that can generate instrumental tracks to use in videos for free without worrying about copyright claims, TechCrunch reports. The new feature was demonstrated in a video posted on the company’s Creator Insider channel this week.",
+    "https://picsum.photos/400?random=23"
+)
+
+news24 = News(
+    "Netflix is testing a new OpenAI-powered search",
+    "The new search engine will let users “look for shows using far more specific terms, including the subscriber’s mood, for example, the company said,” per the report. This OpenAI-powered search will also allow users to make queries that “go well beyond genres or actors’ names.”",
+    "https://picsum.photos/400?random=24"
+)
+
+news_list = [news1, news2, news3, news4, news5, news6, news7, news8, news9, news10, news11, news12, news13, news14, news15, news16, news17, news18, news19, news20, news21, news22, news23, news24]
